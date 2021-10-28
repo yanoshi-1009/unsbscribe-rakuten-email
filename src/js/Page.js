cb782b3f7cc2619acf769e80eace4823eb8bb7f6
yanoshi-1009 <y.idakko@gmail.com>
2021-10-29 01:50:59 +0900

    first init

---
+++ src/js/Page.js
@@ -0,0 +1,107 @@
+// const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
+const puppeteer = chromium.puppeteer;
+
+module.exports = class {
+  constructor(id, pass) {
+    this.url = "https://www.rakuten.co.jp/";
+    this.id = id;
+    this.pass = pass;
+  }
+
+  async init() {
+    this.browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless
+      // headless: false
+    });
+    this.page = await this.browser.newPage();
+    await this.page.goto(this.url);
+  }
+
+  async login() {
+    const buttons = await this.page.$$("button");
+    const targetBtn = buttons[2];
+
+    const isLoginBtn = await targetBtn.evaluate((button) => {
+      if (button.ariaLabel === "ログイン") {
+        return true;
+      }
+      return false;
+    });
+
+    if (!isLoginBtn) {
+      return;
+    }
+
+    await Promise.all([
+      this.page.waitForSelector("#loginInner_u"),
+      this.page.waitForSelector("#loginInner_p"),
+      this.page.waitForSelector(".loginButton"),
+      targetBtn.click()
+    ]);
+
+    await this.page.$eval(
+      "#loginInner_u",
+      (elm, id) => {
+        elm.value = id;
+      },
+      this.id
+    );
+    await this.page.$eval(
+      "#loginInner_p",
+      (elm, pass) => {
+        elm.value = pass;
+      },
+      this.pass
+    );
+
+    const loginBtn = await this.page.$(".loginButton");
+    await Promise.all([this.page.waitForNavigation(), loginBtn.click()]);
+  }
+
+  async goToUnsubscribePage() {
+    await this.page.goto(
+      "https://emagazine.rakuten.co.jp/ns?act=chg_news&f=member"
+    );
+  }
+
+  async unsbscriveFromRakuten() {
+    const allUncheckBtn = await this.page.$("#allUncheck");
+    const registerBtn = await this.page.$("#btnRegister");
+
+    if (!allUncheckBtn) {
+      return;
+    }
+
+    await allUncheckBtn.click();
+    await Promise.all([this.page.waitForNavigation(), registerBtn.click()]);
+  }
+
+  async unsbscriveFromShop() {
+    await this.page.goto(
+      "https://emagazine.rakuten.co.jp/ns?act=chg_rmail&f=member&mflg=0"
+    );
+    const allUncheckBtn = await this.page.$("#allUncheck");
+    const buttons = await this.page.$$("input");
+    const targetBtn = buttons[6];
+
+    if (!allUncheckBtn) {
+      return;
+    }
+
+    await allUncheckBtn.click();
+    await Promise.all([this.page.waitForNavigation(), targetBtn.click()]);
+
+    const buttons2 = await this.page.$$("input");
+    const targetBtn2 = buttons2[3];
+
+    await targetBtn2.click();
+  }
+
+  async close() {
+    this.browser.close();
+  }
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,28 @@
-// import '../style/style.scss';
+const Page = require("./Page");
 
-(() => {
-  'use strict';
-})();
+exports.handler = async () => {
+  "use strict";
+
+  try {
+    const page = new Page(process.env.id, process.env.pass);
+    await page.init();
+
+    await page.login();
+    await page.goToUnsubscribePage();
+
+    await page.unsbscriveFromRakuten();
+    await page.unsbscriveFromShop();
+
+    await page.close();
+
+    return {
+      statusCode: 200,
+      body: "Successfully unsubscribe email from Rakuten"
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error.body)
+    };
+  }
+};
